e7786586a57672320ba9a197d9047efc1f7bfb12
auleu <igor.sella@gmail.com>
2015-04-21 21:19:34 -0300

    Subiu
    eba!

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject foco;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (foco.transform.position.x, foco.transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/scripts/Controlenave.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlenave : MonoBehaviour {
+	float dir=0;
+	float lift=0;
+	Rigidbody2D Rgd;
+	// Use this for initialization
+	void Start () {
+		Rgd = GetComponent<Rigidbody2D> ();
+		lift = 9;
+		Rgd.centerOfMass = new Vector2 (0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dir = Input.GetAxis ("Horizontal");
+		lift = Input.GetAxis ("Vertical")*11;
+		lift = Mathf.Clamp (lift, 0, 11);
+		Debug.Log (lift);
+	}
+	void FixedUpdate () {
+		Rgd.AddRelativeForce (new Vector2 (0, lift));
+		Rgd.AddTorque (dir * -15f);
+		float ang = Vector2.Angle (transform.right, Vector2.up);
+		//Rgd.AddTorque ((ang - 90)*0.025f);
+	}
+}
